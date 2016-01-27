a4a426016e42ffae750dc7217b1d08f019f23d02
Qian He <github@heqian.name>
2016-01-27 16:11:23 -0500

    Add comments for config.js

--- config.js
+++ config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
 	ib: {
-		host: "127.0.0.1",
-		clientId: 200,
-		port: 7496
+		host: "127.0.0.1",	// the IP address of machine running IB Trader Workstation (TWS) or IB Gateway (IBG) 
+		clientId: 200,		// any number
+		port: 7496			// port number, 7496 (default for TWS) or 4002 (default for IBG)
 	},
-	what: "MIDPOINT",
-	size: "1 min"
+	what: "MIDPOINT",		// "TRADES", "BID", "ASK", "MIDPOINT"
+	size: "1 min"			// "1 sec", "5 secs", "15 secs", "30 secs", "1 min", "2 mins", "3 mins", "5 mins", "15 mins", "30 mins", "1 hour", "1 day"
 };
